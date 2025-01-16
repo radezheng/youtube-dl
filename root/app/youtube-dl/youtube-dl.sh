250d7c83c74d29b4cc2ae66231b0de518b22e5b4
radezheng <radezheng@hotmail.com>
2025-01-16 11:07:04 +0800

    entry

--- root/app/youtube-dl/youtube-dl.sh
+++ root/app/youtube-dl/youtube-dl.sh
@@ -8,15 +8,18 @@ if grep -qPe '^--download-archive ' '/config/args.conf'; then youtubedl_args_dow
 youtubedl_binary='yt-dlp'
 exec="$youtubedl_binary"
 exec+=" --config-location '/config/args.conf'"
-exec+=" --batch-file '/tmp/urls'"; (cat '/config/channels.txt'; echo '') > '/tmp/urls.temp'
+# exec+=" --batch-file '/tmp/urls'"; (cat '/config/channels.txt'; echo '') > '/tmp/urls.temp'
 if $youtubedl_args_verbose; then exec+=" --verbose"; fi
 if $youtubedl_args_output_expand; then exec+=" $(grep -Pe '^(--output |-o ).*\$\(' '/config/args.conf')"; fi
 if [ -f '/config/cookies.txt' ]; then exec+=" --cookies '/config/cookies.txt'"; fi
-if $youtubedl_subscriptions; then echo 'https://www.youtube.com/feed/channels' >> '/tmp/urls.temp'; fi
-if $youtubedl_watchlater; then echo ":ytwatchlater | --playlist-end '-1' --no-playlist-reverse" >> '/tmp/urls.temp'; fi
+# if $youtubedl_subscriptions; then echo 'https://www.youtube.com/feed/channels' >> '/tmp/urls.temp'; fi
+# if $youtubedl_watchlater; then echo ":ytwatchlater | --playlist-end '-1' --no-playlist-reverse" >> '/tmp/urls.temp'; fi
 if ! $youtubedl_args_format; then exec+=" --format '$(cat '/config.default/format')'"; fi
 if ! $youtubedl_args_download_archive; then exec+=" --download-archive '/config/archive.txt'"; fi
 
+echo $URL
+exec+=" $URL"
+echo $exec
 if [ -f '/config/pre-execution.sh' ]
 then
   echo '[pre-execution] running pre-execution script...'
@@ -31,22 +34,24 @@ echo ''; echo "$(date '+%Y-%m-%d %H:%M:%S') - starting execution"
 
 if $youtubedl_lockfile; then touch '/downloads/.youtubedl-running' && rm -f '/downloads/.youtubedl-completed'; fi
 
-while [ -f '/tmp/urls.temp' ]
-do
-  extra_url_args=''
-  sed -i '/^#/d' '/tmp/urls.temp'
-  if grep -qPe '\|' '/tmp/urls.temp'
-  then
-    grep -m 1 -nPe '\|' '/tmp/urls.temp' > '/tmp/urls'
-    sed -i -E "$(grep -oPe '^[0-9]+' /tmp/urls)d" '/tmp/urls.temp'
-    extra_url_args="$(grep -oPe '.*?\|\K.*' '/tmp/urls')"
-    sed -i -E 's!([0-9]*:)(.*) (\|.*)!\2!' '/tmp/urls'
-  else
-    mv '/tmp/urls.temp' '/tmp/urls'
-  fi
-  eval "$exec $extra_url_args"
-  rm -f '/tmp/urls'
-done
+# while [ -f '/tmp/urls.temp' ]
+# do
+#   extra_url_args=''
+#   sed -i '/^#/d' '/tmp/urls.temp'
+#   if grep -qPe '\|' '/tmp/urls.temp'
+#   then
+#     grep -m 1 -nPe '\|' '/tmp/urls.temp' > '/tmp/urls'
+#     sed -i -E "$(grep -oPe '^[0-9]+' /tmp/urls)d" '/tmp/urls.temp'
+#     extra_url_args="$(grep -oPe '.*?\|\K.*' '/tmp/urls')"
+#     sed -i -E 's!([0-9]*:)(.*) (\|.*)!\2!' '/tmp/urls'
+#   else
+#     mv '/tmp/urls.temp' '/tmp/urls'
+#   fi
+#   eval "$exec $extra_url_args"
+#   rm -f '/tmp/urls'
+# done
+
+eval "$exec"
 
 if $youtubedl_lockfile; then touch '/downloads/.youtubedl-completed' && rm -f '/downloads/.youtubedl-running'; fi
 
--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -2,6 +2,13 @@
 
 echo "[startup] Running init scripts..."
 
+set -x
+echo "--------------Start.sh--------------"
+git clone https://${GitToken}@github.com/radezheng/youtube-dl.git /app/youtube-dl
+
+cp -f /app/youtube-dl/root/app/youtube-dl.sh /app/youtube-dl/youtube-dl.sh
+chmod +x /app/youtube-dl/youtube-dl.sh
+
 for init in $(ls /etc/cont-init.d/); do
   /etc/cont-init.d/$init
 done
